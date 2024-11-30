609152bd79c1be57b69c9fcf2a6a567f54d82c28
SzymonWegrzynek <u105_szyweg_waw@technischools.com>
2024-11-30 10:30:22 +0100

    first neural network

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use activations::SIGMOID;
+use network::Network;
+
+mod activations;
+mod matrix;
+mod network;
+
 fn main() {
-    println!("Hello, world!");
+    let inputs = vec![
+        vec![0.0, 0.0],
+        vec![0.0, 1.0],
+        vec![1.0, 0.0],
+        vec![1.0, 1.0],
+    ];
+
+    let targets = vec![vec![0.0], vec![1.0], vec![1.0], vec![0.0]];
+
+    let mut network = Network::new(vec![2, 3, 1], 0.5, SIGMOID);
+    network.train(inputs.clone(), targets.clone(), 10000);
+
+    println!("0 and 0: {:?}", network.feed_forward(vec![0.0, 0.0]));
+    println!("0 and 1: {:?}", network.feed_forward(vec![0.0, 1.0]));
+    println!("1 and 0: {:?}", network.feed_forward(vec![1.0, 0.0]));
+    println!("1 and 1: {:?}", network.feed_forward(vec![1.0, 1.0]));
 }
